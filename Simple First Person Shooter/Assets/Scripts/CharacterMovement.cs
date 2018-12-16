e08d9aa20ceddf9a9b77f49966829fda26c7193e
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-12-16 19:48:05 +0000

    Wireframe Projects - Simple First-Person Shooter - Done.
    This was taken from issue 3 of Wireframe.
    
    It was simple and the tutorial didn't really teach that much but there was some syntax I hadn't seen before so I added that to my list.
    There was a weird bug with the object not shooting but I eventually found the inn-editor error and fixed it.

---
+++ Simple First Person Shooter/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float speed = 5;
+    public float jumpPower = 4;
+    Rigidbody rb;
+    CapsuleCollider col;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get the input value from the controllers.
+        float Horizontal = Input.GetAxis("Horizontal") * speed;
+        float Vertical = Input.GetAxis("Vertical") * speed;
+        Horizontal *= Time.deltaTime;
+        Vertical *= Time.deltaTime;
+        //Translate our character via our inputs.
+        transform.Translate(Horizontal, 0, Vertical);
+
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+            //Add upward force to the rigidbody when we press jump.
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    private bool isGrounded()
+    {
+        //Test that we are grounded by drawing an invisible line (raycast).
+        //If this hits a solid object e.g. the floor, then we are are grounded.
+        return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
+    }
+}
